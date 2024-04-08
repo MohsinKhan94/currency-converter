9d734c983fb713f3ba98d62fb32dbd15cbc2a150
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-04-08 21:41:02 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer'
+
+const currency:any = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280     
+};
+
+async function main() {
+    let user_Answer = await inquirer.prompt([
+        {
+            name:'from',
+            message:'Enter From Currency',
+            type:'list',
+            choices:['USD','EUR','GBP','INR','PKR']
+        },
+        {
+            name:'to',
+            message:'Enter To Currency',
+            type:'list',
+            choices:['USD','EUR','GBP','INR','PKR']
+        },
+        {
+            name:'amount',
+            message:'Enter Your Amount',
+            type:'number'
+        }
+    ]);
+
+    let fromAmount = currency[user_Answer.from]
+    let toAmount = currency[user_Answer.to]
+    let amount = user_Answer.amount;
+    let baseAmount = amount / fromAmount;
+    let convertedAmount = baseAmount * toAmount;
+    convertedAmount = Math.round(convertedAmount); 
+    console.log(convertedAmount);
+}
+
+main();
+
+
